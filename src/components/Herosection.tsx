ae1e8de45593bc36fc03b81894a2c29c2b9f870e
vanshsharmaCS <vanshsharma_cs22@gla.ac.in>
2024-03-28 22:16:31 +0530

    first

---
+++ src/components/Herosection.tsx
@@ -0,0 +1,80 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "./ui/hero-parallax";
+import Image from "next/image";
+
+export const products = [
+    {
+        title: "CodeForces",
+        link: "https://codeforces.com/profile/vansh9927",
+        thumbnail:
+        "/codeforces.png",
+    },
+    {
+        title: "LeetCode",
+        link: "https://leetcode.com/zephyrus17/",
+        thumbnail:
+        "/leetcode.png"
+    },
+    
+    {
+        title: "GitHub",
+        link: "https://github.com/vanshsharmaCS",
+        thumbnail:
+        "/github.png", 
+    },
+    {
+      title: "LinkedIn",
+      link: "https://www.linkedin.com/in/vansh-sharma-2954a2246/",
+      thumbnail:
+        "/linkedin.png",
+    },
+    {
+      title: "Editrix AI",
+      link: "https://editrix.ai",
+      thumbnail:
+        "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+    },
+    {
+      title: "Pixel Perfect",
+      link: "https://app.pixelperfect.quest",
+      thumbnail:
+        "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+    },
+   
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      thumbnail:
+        "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+    },
+    {
+      title: "Aceternity UI",
+      link: "https://ui.aceternity.com",
+      thumbnail:
+        "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+    },
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      thumbnail:
+        "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+    },
+    {
+      title: "SmartBridge",
+      link: "https://smartbridgetech.com",
+      thumbnail:
+        "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+    },
+  
+   
+    
+  ];
+
+const Herosection = () => {
+  return (
+    <HeroParallax products={products} />
+  )
+}
+
+export default Herosection
