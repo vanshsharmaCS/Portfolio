8d8eb81457f075387ffd4c65f669c508514453d4
vanshsharmaCS <vanshsharma_cs22@gla.ac.in>
2024-03-28 22:26:15 +0530

    f

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -146,7 +146,7 @@ export const ProductCard = ({
           src={product.thumbnail}
           height="600"
           width="600"
-          className="object-cover object-left-top absolute h-full w-full inset-0"
+          className="object-cover object-left-top absolute md:h-full md:w-full h-[20rem] w-[30rem] m-5 inset-0"
           alt={product.title}
         />
       </Link>
